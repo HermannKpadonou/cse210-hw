bc787f73f34a7f849c371d9de77ad1ab7eb951d5
HermannKpadonou~ <kpadonouhermann@gmail.com>
2025-09-11 23:59:46 +0000

    Exercice 05 is done and uploaded!

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
+
+    static void DipslayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber(int unused)
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int userNumber)
+    {
+        return userNumber * userNumber;
+    }
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DipslayWelcome();
+
+        string userName = PromptUserName();
+
+        int userNumber = PromptUserNumber(0);
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
     }
 }
