9893f3345ed8caa4334965452f68e96c56ba2e60
HermannKpadonou~ <kpadonouhermann@gmail.com>
2025-09-11 17:24:50 +0000

    Exercice 03 is done and uploaded

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 100);
+        string input;
+
+        Console.Write("What is your guess? ");
+        input = Console.ReadLine();
+        int guess = int.Parse(input);
+
+        if (guess < magicNumber)   
+        {
+            Console.WriteLine("Lower!");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Higher!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
