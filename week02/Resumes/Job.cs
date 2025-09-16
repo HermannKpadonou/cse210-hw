ca18510607394e0b1cbdd9a3c05bf00423f3eb25
HermannKpadonou~ <kpadonouhermann@gmail.com>
2025-09-16 18:03:40 +0000

    I pushed my code to GitHub

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+using System;
+public class Job
+{
+    public string _jobCompany;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle}({_jobCompany}){_startYear}-{_endYear}");
+    }
+
+}
