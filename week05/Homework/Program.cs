fde2ed5f9ef4f16ecd7084bdd623bac0e8b739b0
HermannKpadonou~ <kpadonouhermann@gmail.com>
2025-10-07 17:50:00 +0000

    update my code

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Alice Carson", "Math");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Bertin Agbodjovi", "Physics", "Section 7.3", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Merveille Mutombo", "English", "The Great changes in History");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
