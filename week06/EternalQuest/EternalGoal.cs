a5ab54ba9b3ddb6256027f8a19ff9c0b1d480a5c
HermannKpadonou~ <kpadonouhermann@gmail.com>
2025-10-15 14:30:11 +0000

    Push on my assignment

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace EternalQuest
+{
+    public class EternalGoal : Goal
+    {
+        public EternalGoal(string name, string description, int points) : base(name, description, points)
+        {
+        }
+
+        public override int RecordEvent()
+        {
+            return _points; 
+        }
+
+        public override string GetDetailsString()
+        {
+            return $"[∞] {_name} ({_description})";
+        }
+
+        public override string GetStringRepresentation()
+        {
+            return $"EternalGoal:{_name},{_description},{_points}";
+        }
+    }
+}
---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace EternalQuest
+{
+    public abstract class Goal
+    {
+        protected string _name;
+        protected string _description;
+        protected int _points;
+        protected bool _isComplete;
+
+        public Goal(string name, string description, int points)
+        {
+            _name = name;
+            _description = description;
+            _points = points;
+            _isComplete = false;
+        }
+
+        public string Name => _name;
+        public string Description => _description;
+        public int Points => _points;
+        public bool IsComplete => _isComplete;
+
+        public abstract int RecordEvent();
+        public abstract string GetDetailsString();
+        public abstract string GetStringRepresentation();
+       
+    }
+}
